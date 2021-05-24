22822c5aad060ab7e9651764ab5e431ef769a55b
yuto90 <solidusgako4087@gmail.com>
2021-05-24 21:48:58 +0900

    詳細ページ作成

--- config/memos/urls.py
+++ config/memos/urls.py
@@ -6,4 +6,5 @@ app_name = 'memos'
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('detail/<int:memo_id>/', views.detail, name='detail'),
 ]
--- config/memos/views.py
+++ config/memos/views.py
@@ -5,5 +5,11 @@ from .models import Memo
 
 
 def index(request):
+    # メモのクエリセットを全て降順に取得
     memos = Memo.objects.order_by('-created_datetime')
     return render(request, 'memos/index.html', {'memos': memos})
+
+
+def detail(request, memo_id):
+    memo = Memo.objects.get(id=memo_id)
+    return render(request, 'memos/detail.html', {'memo': memo})
